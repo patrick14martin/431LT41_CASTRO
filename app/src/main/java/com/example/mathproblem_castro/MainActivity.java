341c3ef30afaa22e042c684504025466324baaeb
Patrick Castro <08psuit08@gmail.com>
2022-11-21 22:44:38 +0800

    1123

--- app/src/main/java/com/example/mathproblem_castro/MainActivity.java
+++ app/src/main/java/com/example/mathproblem_castro/MainActivity.java
@@ -6,84 +6,136 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
+    Integer val1,val2,val3,add1;
+    EditText first,second,third;
 
-    private TextView textout;
+
 
     public void sum(View v) {
 
-        EditText first = findViewById(R.id.etNumber1);
-        EditText second = findViewById(R.id.etNumber2);
-        EditText third = findViewById(R.id.etNumber3);
+        first = findViewById(R.id.etNumber1);
+        second = findViewById(R.id.etNumber2);
+        third = findViewById(R.id.etNumber3);
+
         Button add = findViewById(R.id.button1);
-        String a = first.getText().toString();
-        String b = second.getText().toString();
-        String c = third.getText().toString();
-        Integer val1 = Integer.parseInt(a);
-        Integer val2 = Integer.parseInt(b);
-        Integer val3 = Integer.parseInt(c);
-        Integer add1 = val1 + val2 + val3;
-        String aw = String.valueOf(add1);
-        textout = (TextView)findViewById(R.id.textView7);
-        textout.setText(aw);
+
+            String a = first.getText().toString();
+            String b = second.getText().toString();
+            String c = third.getText().toString();
+            if (first.getText().toString().isEmpty() || second.getText().toString().isEmpty() || third.getText().toString().isEmpty()) {
+                val1 = 0;
+                val2 = 0;
+                val3 = 0;
+            } else {
+                val1 = Integer.parseInt(a);
+                val2 = Integer.parseInt(b);
+                val3 = Integer.parseInt(c);
+            }
+            add1 = val1 + val2 + val3;
+        String resultMessage = String.valueOf(add1);
+        Bundle args = new Bundle();
+        args.putString("result", resultMessage);
+        DialogFragmentCustom dialogFragmentImp = new DialogFragmentCustom();
+        dialogFragmentImp.setArguments(args);
+        dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+        clearEditText();
+
+
+
 
 
     }
     public void difference(View v){
 
-        EditText first = findViewById(R.id.etNumber1);
-        EditText second = findViewById(R.id.etNumber2);
-        EditText third = findViewById(R.id.etNumber3);
+        first = findViewById(R.id.etNumber1);
+        second = findViewById(R.id.etNumber2);
+        third = findViewById(R.id.etNumber3);
         Button add2 = findViewById(R.id.button2);
         String a = first.getText().toString();
         String b = second.getText().toString();
         String c = third.getText().toString();
-        Integer val1 = Integer.parseInt(a);
-        Integer val2 = Integer.parseInt(b);
-        Integer val3 = Integer.parseInt(c);
-        Integer add1 = val1 - val2 - val3;
-        String aw = String.valueOf(add1);
-        textout = (TextView)findViewById(R.id.textView7);
-        textout.setText(aw);
+        if (first.getText().toString().isEmpty() || second.getText().toString().isEmpty() || third.getText().toString().isEmpty()) {
+            val1 = 0;
+            val2 = 0;
+            val3 = 0;
+        } else {
+            val1 = Integer.parseInt(a);
+            val2 = Integer.parseInt(b);
+            val3 = Integer.parseInt(c);
+        }
+         add1 = val1 - val2 - val3;
+        String resultMessage = String.valueOf(add1);
+        Bundle args = new Bundle();
+        args.putString("result", resultMessage);
+        DialogFragmentCustom dialogFragmentImp = new DialogFragmentCustom();
+        dialogFragmentImp.setArguments(args);
+        dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+        clearEditText();
 
     }
     public void product(View v){
-        EditText first = findViewById(R.id.etNumber1);
-        EditText second = findViewById(R.id.etNumber2);
-        EditText third = findViewById(R.id.etNumber3);
+        first = findViewById(R.id.etNumber1);
+        second = findViewById(R.id.etNumber2);
+        third = findViewById(R.id.etNumber3);
         Button add3 = findViewById(R.id.button3);
         String a = first.getText().toString();
         String b = second.getText().toString();
         String c = third.getText().toString();
-        Integer val1 = Integer.parseInt(a);
-        Integer val2 = Integer.parseInt(b);
-        Integer val3 = Integer.parseInt(c);
-        Integer add1 = val1 * val2 * val3;
-        String aw = String.valueOf(add1);
-        textout = (TextView)findViewById(R.id.textView7);
-        textout.setText(aw);
+        if (first.getText().toString().isEmpty() || second.getText().toString().isEmpty() || third.getText().toString().isEmpty()) {
+            val1 = 0;
+            val2 = 0;
+            val3 = 0;
+        } else {
+            val1 = Integer.parseInt(a);
+            val2 = Integer.parseInt(b);
+            val3 = Integer.parseInt(c);
+        }
+         add1 = val1 * val2 * val3;
+        String resultMessage = String.valueOf(add1);
+        Bundle args = new Bundle();
+        args.putString("result", resultMessage);
+        DialogFragmentCustom dialogFragmentImp = new DialogFragmentCustom();
+        dialogFragmentImp.setArguments(args);
+        dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+        clearEditText();
 
     }
     public void quotient(View v){
-        EditText first = findViewById(R.id.etNumber1);
-        EditText second = findViewById(R.id.etNumber2);
-        EditText third = findViewById(R.id.etNumber3);
+        first = findViewById(R.id.etNumber1);
+        second = findViewById(R.id.etNumber2);
+        third = findViewById(R.id.etNumber3);
         Button add4 = findViewById(R.id.button4);
         String a = first.getText().toString();
         String b = second.getText().toString();
         String c = third.getText().toString();
-        Integer val1 = Integer.parseInt(a);
-        Integer val2 = Integer.parseInt(b);
-        Integer val3 = Integer.parseInt(c);
-        Integer add1 = val1 / val2 / val3;
-        String aw = String.valueOf(add1);
-        textout = (TextView)findViewById(R.id.textView7);
-        textout.setText(aw);
+        if (first.getText().toString().isEmpty() || second.getText().toString().isEmpty() || third.getText().toString().isEmpty()) {
+            val1 = 0;
+            val2 = 0;
+            val3 = 0;
+        } else {
+            val1 = Integer.parseInt(a);
+            val2 = Integer.parseInt(b);
+            val3 = Integer.parseInt(c);
+        }
+        add1 = val1 / val2 / val3;
+        String resultMessage = String.valueOf(add1);
+        Bundle args = new Bundle();
+        args.putString("result", resultMessage);
+        DialogFragmentCustom dialogFragmentImp = new DialogFragmentCustom();
+        dialogFragmentImp.setArguments(args);
+        dialogFragmentImp.show(getSupportFragmentManager(),"Display Result");
+        clearEditText();
     }
-
+    public void clearEditText(){
+        first.getText().clear();
+        second.getText().clear();
+        third.getText().clear();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
